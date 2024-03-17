b0698d0c020070a840fa494454071534955050da
Harsh Soni <harshsoni13112003@gmail.com>
2024-03-17 14:28:05 +0530

    Minor updates

--- src/components/GithubPopularRepos/index.js
+++ src/components/GithubPopularRepos/index.js
@@ -1,58 +1,57 @@
-import { Component } from "react";
-import { ThreeDots } from "react-loader-spinner";
-import LanguageFilterItem from "../LanguageFilterItem";
-import RepositoryItem from "../RepositoryItem";
+import {Component} from 'react'
+import Loader from 'react-loader-spinner'
+import LanguageFilterItem from '../LanguageFilterItem'
+import RepositoryItem from '../RepositoryItem'
 
-import "./index.css";
+import './index.css'
 
 const languageFiltersData = [
-  { id: "ALL", language: "All" },
-  { id: "JAVASCRIPT", language: "Javascript" },
-  { id: "RUBY", language: "Ruby" },
-  { id: "JAVA", language: "Java" },
-  { id: "CSS", language: "CSS" },
-];
+  {id: 'ALL', language: 'All'},
+  {id: 'JAVASCRIPT', language: 'Javascript'},
+  {id: 'RUBY', language: 'Ruby'},
+  {id: 'JAVA', language: 'Java'},
+  {id: 'CSS', language: 'CSS'},
+]
 
 class GithubPopularRepos extends Component {
-  state = { selectedId: "ALL", isLoaded: false, apiError: false };
+  state = {selectedId: 'ALL', isLoaded: false, apiError: false}
 
-  repos = [];
+  repos = []
 
   componentDidMount() {
-    this.fetchItems();
+    this.fetchItems()
   }
 
-  filterClick = (id) => {
+  filterClick = id => {
     this.setState(
-      { isLoaded: false, selectedId: id, apiError: false },
-      this.fetchItems
-    );
-  };
+      {isLoaded: false, selectedId: id, apiError: false},
+      this.fetchItems,
+    )
+  }
 
   fetchItems = async () => {
-    const { selectedId } = this.state;
-    const githubReposApiUrl = `https://apis.ccbp.in/popular-repos?language=${selectedId}`;
-    try {
-      const response = await fetch(githubReposApiUrl);
-      if (response.status === 200) {
-        const data = await response.json();
-        this.repos = data.popular_repos;
-      } else {
-        this.setState({ apiError: true });
-      }
-      this.setState({ isLoaded: true });
-    } catch (e) {
-      this.setState({ apiError: true , isLoaded: true});
+    const {selectedId} = this.state
+    const githubReposApiUrl = `https://apis.ccbp.in/popular-repos`
+    const response = await fetch(
+      `${githubReposApiUrl}?language=${selectedId}`,
+      {method: 'GET'},
+    )
+    if (response.status === 200) {
+      const data = await response.json()
+      this.repos = data.popular_repos
+    } else {
+      this.setState({apiError: true})
     }
-  };
+    this.setState({isLoaded: true})
+  }
   /* eslint-disable */
   render() {
-    const { selectedId, isLoaded, apiError } = this.state;
+    const {selectedId, isLoaded, apiError} = this.state
     return (
       <div className="PopularRepos">
         <h1 className="heading">Popular</h1>
         <ul>
-          {languageFiltersData.map((obj) => (
+          {languageFiltersData.map(obj => (
             <LanguageFilterItem
               selected={obj.id === selectedId}
               key={obj.id}
@@ -63,16 +62,7 @@ class GithubPopularRepos extends Component {
         </ul>
         {isLoaded === false ? (
           <div data-testid="loader">
-            <ThreeDots
-              visible={true}
-              height="80"
-              width="80"
-              color="#4fa94d"
-              radius="9"
-              ariaLabel="three-dots-loading"
-              wrapperStyle={{}}
-              wrapperClass=""
-            />
+            <Loader type="ThreeDots" color="#0284c7" height={80} width={80} />
           </div>
         ) : apiError ? (
           <div>
@@ -84,14 +74,14 @@ class GithubPopularRepos extends Component {
           </div>
         ) : (
           <ul className="repos">
-            {this.repos.map((obj) => (
+            {this.repos.map(obj => (
               <RepositoryItem key={obj.id} itemDetails={obj} />
             ))}
           </ul>
         )}
       </div>
-    );
+    )
   }
 }
 
-export default GithubPopularRepos;
+export default GithubPopularRepos
